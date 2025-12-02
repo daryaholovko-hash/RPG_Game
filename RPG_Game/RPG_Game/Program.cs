98998cf0a846cc65c0ab460e128e25d8725bee8b
daryaholovko-hash <daryaholovko@gmail.com>
2025-12-02 13:04:20 +0200

    Add Character and IEquippable

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,5 +1,18 @@
 ﻿namespace RPG_Game
 {
+    public interface IEquippable
+    {
+        string Name { get; }
+        void Equip();
+
+        void Unequip();
+    public abstract class Characer
+    {
+         private string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _strength;
+    }
     internal class Program
     {
         static void Main(string[] args)
